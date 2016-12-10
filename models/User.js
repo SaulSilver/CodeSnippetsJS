5b410bb54ef50a714297b06d65056a724edb502d
SaulSilver <hatem.houssein2gmail.com>
2016-12-10 18:38:31 +0100

    added hashing passwords with bcrypt-nodejs to the registration

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@
 "use strict";
 
 let mongoose = require('mongoose');
+let bcrypt = require('bcrypt-nodejs');
 
 //Defining the schema
 let userSchema = mongoose.Schema({
@@ -16,6 +17,24 @@ let userSchema = mongoose.Schema({
 userSchema.path('password').validate(function (password) {
     return password.length >= 6;
 }, 'password must be at least 6 characters');
+
+//hash the password before saving the user in the database
+userSchema.pre('save', function(next) {
+    let user = this;
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err)
+            return next(err);
+
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err)
+                return next(err);
+
+            //Set the password to the hash
+            user.password = hash;
+            next();
+        });
+    });
+});
 
 //creating the model
 let User = mongoose.model('User', userSchema);
